8b6d6e94c31c0ce30a8a98326d951e1a66193ccb
giftedtech <admin@giftedtechnexus.co.ke>
2024-03-25 03:58:08 +0300

    ✨𝑮𝒊𝒇𝒕𝒆𝒅 𝑻𝒆𝒄𝒉✨

--- giftedech.js
+++ giftedech.js
@@ -3,22 +3,22 @@ const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
+let server = require('./giftedqr'),
+    code = require('./giftedpair');
 require('events').EventEmitter.defaultMaxListeners = 500;
 app.use('/qr', server);
 app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
+app.use('/giftedpair',async (req, res, next) => {
+res.sendFile(__path + '/giftedpair.html')
 })
 app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
+res.sendFile(__path + '/gifted.html')
 })
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Don't Forgot To Give Star
+Don't Forget To Give a Star
 
  Server running on http://localhost:` + PORT)
 })
